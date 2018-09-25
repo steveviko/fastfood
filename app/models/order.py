e91e79d81d24eb96188c3a7100bea3237558d0e1
steveviko <stevenviko@gmail.com>
2018-09-25 04:30:41 +0300

    Modify all endpoints [Deliveries #160745133]

--- app/models/order.py
+++ app/models/order.py
@@ -1,34 +1,54 @@
-from flask import Flask, jsonify, request, json,Blueprint
+from flask import Flask, jsonify, request, json
 
 
 
-class FastFoodOrders():
-   
-    def __init__(self):        
-        self.listofOrders = []
-        
-  
+class Menu:
+    menu_item_list = []
+    def add_menu_item(self, menu):
+        menu["item_id"] = (len(self.menu_item_list) + 1)
+        self.menu_item_list.append(menu)
+        return menu
+
+    def get_item_on_menu(self):
+        return self.menu_item_list
+
+    def item_name(self, id):
+        for item_id in self.menu_item_list:
+            if item_id["item_id"] == id:
+                return item_id["name"]
+
+
+class Order(Menu):
+    
+    order_list = []
+    def add(self, order):
+        order["order_id"] = (len(self.order_list) + 1)
+        order["status"] = "Pending"
+        self.order_list.append(order)
+        return order
+
     def get_all(self):
-        """This endpoint will fetch all orders """
-        listofOrders = []
-        if listofOrders == []:
-            return jsonify({"message":"The orders list is currently empty"}), 404
-        for order in listofOrders:           
-            listofOrders.append(order)
-        return jsonify(listofOrders), 200
-        
-    def get_order_by_id(self,orderId):
-        orders =[order for order in self.listofOrders if order['id']== orderId]
-        if  orders:
-            return jsonify({'order':orders[0]}), 200 
-        return jsonify({"message": "The order does not exist "}), 404
+        order_list = self.order_list[:]
+        for item in order_list:
+            if isinstance(item["item"], int):
+                item_names = self.item_name(item["item"])
+                item["item"] = item_names
+                return order_list
+            else:
+                return order_list
 
-    def add_orders(self):
-        data= request.data
-        results  =json.loads(data)      
-        self.listofOrders.append(results) 
-        if not results:              
-            return "No value found"  
-        return jsonify(self.listofOrders)
-            
+    def get_order_by_id(self, orderId):
+        for order in self.order_list:
+            if order["order_id"] == orderId:
+                item_name = self.item_name(order["item"])
+                order["item"] = item_name
+            return order
+
+    def update_order(self, orderId, status):
+        update_order = [status for status in self.order_list if status["order_id"] == orderId]
+        if update_order:
+            update_order[0]["status"] = status
+            return update_order
+        else:
+            return "does not exist"
                     
--- app/views.py
+++ app/views.py
@@ -2,26 +2,52 @@ from flask import Flask, jsonify, make_response,json, request
 
 from app import create_app
 
-from  .models.order import FastFoodOrders
+from  .models.order import Order,Menu
+
+orders = Order()
+menus = Menu()
 
 
 
-orders = FastFoodOrders()
 
 app = create_app()
 
 @app.route("/api/v1/orders", methods =["GET"])
 def get_all_orders():
-    return orders.get_all()
+    if orders.order_list == []:
+        return jsonify({"orders": "No orders Available"}), 400
+    list_of_orders = orders.get_all()
+    return jsonify({'orders': list_of_orders}), 200
 
 @app.route("/api/v1/orders/<int:orderId>", methods =["GET"])
 def get_one_order(orderId):
-    return orders.get_order_by_id(orderId)
+    if isinstance(orderId,int):
+        single_order = orders.get_order_by_id(orderId)
+        return jsonify({"order": single_order}),200
+    else:
+        return jsonify({"Message": " Error,Please fill out correct  id"}), 400
 
 @app.route("/api/v1/orders", methods =["POST"])
-def post_order():    
-   return orders.add_orders()
-    
+def post_order(): 
+    request_data=request.data
+    results  =json.loads(request_data)
+    order = {
+        "name": results["name"],
+        "item": results["item"],
+        "amount": results["amount"]
+        
+    }
+    if order["name"] == "" or order["amount"] == "" or order["item"] == "":
+        return jsonify({"message": "Error,order is not complete "}), 400
+    else:
+        orders.add(order)
+        return jsonify({'Order': order }), 201
 
-
+@app.route("/api/v1/orders/<int:orderId>", methods = ["PUT"])
+def Edit_order(orderId):
+    request_data=request.data
+    results  =json.loads(request_data)
+    order_status = results["status"]
+    updates = orders.update_order(int(orderId), order_status)
+    return jsonify({"order": updates}), 201
 
