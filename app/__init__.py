21298fcb4f069e12c0df56061bfe6fc443227da0
steveviko <stevenviko@gmail.com>
2018-10-04 04:31:41 +0300

    Add post order endpoints[modify 160532692]

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
-
+from app.views import app
 
 from .db import DatabaseConnection
   
--- app/views.py
+++ app/views.py
@@ -1,4 +1,26 @@
+from flask import request, jsonify, make_response,json
+
+from app import app
+from app.db import DatabaseConnection,Order,Menu,Users
+from app.validate import Validation
 
 
 
+orders = Order()
+menus = Menu()
+user = Users()
+db=DatabaseConnection()
+db.create_menu_table()
+db.create_user_table()
+db.create_orders_table()
+validate = Validation()
 
+@app.route('/api/v2/orders', methods=['POST'])
+def create_order():
+    data = request.data
+    results  =json.loads(data)    
+    amount = results['amount']
+    ordered_at = results['ordered_at']
+    status = results['status']
+    insert_one= orders.post_order(amount, ordered_at,status)
+    return jsonify({'order': insert_one})
