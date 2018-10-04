4b3f0e734bd7e22081dc9d703da2a9affa1942c7
steveviko <stevenviko@gmail.com>
2018-10-04 05:55:20 +0300

    Add and Implement user activies

---
+++ app/user_activities.py
@@ -0,0 +1,16 @@
+from .user import User
+from .db import DatabaseConnection
+from werkzeug.security import generate_password_hash, check_password_hash
+
+user = User()
+datab= DatabaseConnection()
+
+class UserActivities:
+    
+    def user_register(self, email, password):
+        user.email = email
+        user.password = password
+        hash_password = generate_password_hash(user.password, method='sha256')
+        user_record = datab.register_user(user.email, user.password, hash_password )
+        print("user creation was successful")
+        return {'user': user_record }
